cf845b0b20ebbadcf4b963b7af932191ab1a3d20
JabesRus <jabesgames@gmail.com>
2023-11-22 07:50:19 +0300

    Тестовый файл

---
+++ src/main/java/TestExamples.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TestExamples {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int num = 1;
+        long numUpper = num;
+        short numLower = (short) num;
+        System.out.println(numUpper);
+        System.out.println(numLower);
+
+    }
+}
