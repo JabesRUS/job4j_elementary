103de0079e6f73ebd8594b973d267521efecc05f
JabesRus <jabesgames@gmail.com>
2023-11-23 15:50:29 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,16 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr";
+        names[1] = "John";
+        names[2] = "Vasiliy";
+        names[3] = "Kolyan";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+
     }
 }
