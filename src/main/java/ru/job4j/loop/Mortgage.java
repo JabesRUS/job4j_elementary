0568057ca9fbe8383be08933cccdeba8c4dbdfd3
JabesRus <jabesgames@gmail.com>
2023-11-24 11:07:48 +0300

    5.7. Ипотека [#156315 #461994]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        while (amount * percent / 100 > 0) {
+        while (amount > 0) {
             year++;
-            amount = amount + amount * percent / 100 - salary;
+            amount += amount * percent / 100 - salary;
         }
         return year;
     }
